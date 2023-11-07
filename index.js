15a22527a7d13e24cf2248956a9cc4c6d1a2ef9a
Afsana-Tasnim-Juha <111423065+Afsana-Tasnim-Juha@users.noreply.github.com>
2023-11-07 19:34:59 +0100

    create a new post and save data to the server

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 
@@ -30,15 +30,39 @@ async function run() {
     try {
 
         const productCollection = client.db("productDB").collection("collect");
+        const CardCollection = client.db("CardDB").collection("collectCard");
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-
+        //addProduct
         app.post('/product', async (req, res) => {
             const newProduct = req.body;
             console.log(newProduct);
             const result = await productCollection.insertOne(newProduct);
             res.send(result);
         })
+
+        app.get('/product', async (req, res) => {
+            const cursor = productCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const newProduct = await productCollection.findOne(query);
+
+            res.send(newProduct);
+        })
+
+        app.post('/card', async (req, res) => {
+            const details = req.body;
+            console.log(details);
+            const result2 = await CardCollection.insertOne(details);
+            res.send(result2);
+        })
+
+
+
 
 
         // Send a ping to confirm a successful connection
